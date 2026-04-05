9d311798c43a6f313015233e67a3f23efee4cc0d
engmohamedgamal733-boop <eng.mohamed.gamal733@gmail.com>
2026-04-05 16:57:16 +0200

    Add files via upload

---
+++ memories/data.js
@@ -0,0 +1,33 @@
+const memoriesData = [
+    {
+        type: "image",
+        file: "memories/images/me.jpg",
+        emoji: "💕",
+        date: "1 يناير 2025",
+        title: "أول يوم اتقابلنا",
+        description: "ده كان أحلى يوم في حياتي... فاكرة لما شوفتك أول مرة؟ 💕"
+    },
+    {
+        type: "video",
+        file: "memories/videos/me.mp4",
+        emoji: "🎂",
+        date: "15 مارس 2025",
+        title: "عيد ميلادك 🎈",
+        description: "أحلى عيد ميلاد لأحلى إنسانة! كل سنة وانتي طيبة يا قمر 🎉"
+    },
+    {
+        type: "text",
+        emoji: "😂",
+        date: "20 فبراير 2025",
+        title: "لما قولتيلي متفقريش 🤣",
+        description: "فاكرة لما قولتيلي عايزة أبعد فترة وأعمل بلوك؟ وأنا قولتلك متفقريش 😂😂 ده كان موقف يضحك أوي!"
+    },
+    {
+        type: "image",
+        file: "memories/images/screenshot1.jpg",
+        emoji: "📱",
+        date: "10 فبراير 2025",
+        title: "سكرين محادثة تضحك 😆",
+        description: "ده من أكتر المحادثات اللي ضحكتني معاكي 🤣💕"
+    }
+];
